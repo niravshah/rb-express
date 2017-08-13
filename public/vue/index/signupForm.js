2be9cda06611e8340fb1a431faa5d831cc9bfb38
shahnirav <shah.nirav@me.com>
2017-08-13 22:56:19 +0100

    commit

--- public/vue/index/signupForm.js
+++ public/vue/index/signupForm.js
@@ -1,7 +1,28 @@
 var vSignupForm = new Vue({
     el: '#signupForm',
     data: {
-        overlayForm: {}
+        amount: '',
+        title: '',
+        fname: '',
+        lname: '',
+        email: '',
+        mobile: ''
     },
-    methods: {}
+    computed: {
+        isValid: function () {
+            return !this.errors.any()
+                && this.amount != ''
+                && this.fname!=''
+                && this.lname!=''
+                && this.title != ''
+                && this.email != ''
+                && this.mobile!='';
+        }
+    },
+    methods: {
+        onSubmit: function () {
+            if (!this.errors.any()) {
+            }
+        }
+    }
 });
