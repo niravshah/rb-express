36a47f0155c93918a361fba7e335a53cb0a09359
shahnirav <shah.nirav@me.com>
2017-08-23 21:49:49 +0100

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,16 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
-gulp.task('default', ['scripts'], function () {
+var cleanCSS = require('gulp-clean-css');
+
+
+gulp.task('default', ['minifycss'], function () {
     // place code for your default task here
 });
 
-// Concatenate & Minify JS
-gulp.task('scripts', function () {
-    return gulp.src(['public/vue/libs/vue.js','public/vue/libs/vue-resource.js','public/vue/libs/vee-validate.js','public/vue/plugins/jwt-plugin.js'])
-        .pipe(concat('vue.js'))
-        .pipe(gulp.dest('public/dist'))
-        .pipe(rename('vue.min.js'))
-        .pipe(uglify())
-        .on('error', function (err) {
-            console.log('Error:', err)
-        })
-        .pipe(gulp.dest('public/dist'));
-});
 
-// Watch Files For Changes
-gulp.task('watch', function () {
-    gulp.watch('vue/**/*.js', ['scripts']);
+gulp.task('minifycss', function() {
+    return gulp.src('public/css/custom.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('public/dist'));
 });
