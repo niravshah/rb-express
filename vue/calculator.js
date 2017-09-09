0f2520104f8db37eded17b2d4c5707c2fde1dab6
shahnirav <shah.nirav@me.com>
2017-09-09 11:26:07 +0100

    commit

--- vue/calculator.js
+++ vue/calculator.js
@@ -8,31 +8,31 @@ var vCalculator =
         },
         computed: {
             rb_pf: function () {
-                return this.amountRaised * 0.02
+                return Math.round(this.amountRaised * 0.02);
             },
             rb_cpf: function () {
-                return this.amountRaised * 0.014 + 0.20
+                return Math.round(this.amountRaised * 0.014 + 0.20);
             },
             rb_total: function () {
-                return this.amountRaised - (this.rb_pf + this.rb_cpf)
+                return this.amountRaised - (this.rb_pf + this.rb_cpf);
             },
             jg_pf: function () {
-                return this.amountRaised * 0.05
+                return Math.round(this.amountRaised * 0.05);
             },
             jg_cpf: function () {
-                return this.amountRaised * 0.0125
+                return Math.round(this.amountRaised * 0.0125);
             },
             jg_total: function () {
-                return this.amountRaised - (this.jg_pf + this.jg_cpf)
+                return this.amountRaised - (this.jg_pf + this.jg_cpf);
             },
             gfm_pf: function () {
-                return this.amountRaised * 0.05
+                return Math.round(this.amountRaised * 0.05);
             },
             gfm_cpf: function () {
-                return this.amountRaised * 0.035 + 0.20
+                return Math.round(this.amountRaised * 0.035 + 0.20);
             },
             gfm_total: function () {
-                return this.amountRaised - (this.gfm_pf + this.gfm_cpf)
+                return this.amountRaised - (this.gfm_pf + this.gfm_cpf);
             },
             amountSaved: function () {
                 return (this.rb_total - this.gfm_total);
