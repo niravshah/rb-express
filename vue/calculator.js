fdc1bf5c8574ed9ec804f5739746248d19f96e1c
shahnirav <shah.nirav@me.com>
2017-09-09 11:36:09 +0100

    commit

--- vue/calculator.js
+++ vue/calculator.js
@@ -2,13 +2,18 @@ var vCalculator =
     new Vue({
         el: '#calculator',
         data: {
-            amountRaised: 1500
+            amountRaised: 1500,
+            pf: {
+                rb: 3,
+                jg: 5,
+                gfm: 5
+            }
         },
         created: function () {
         },
         computed: {
             rb_pf: function () {
-                return Math.round(this.amountRaised * 0.02);
+                return Math.round(this.amountRaised * this.pf.rb/100);
             },
             rb_cpf: function () {
                 return Math.round(this.amountRaised * 0.014 + 0.20);
@@ -17,7 +22,7 @@ var vCalculator =
                 return this.amountRaised - (this.rb_pf + this.rb_cpf);
             },
             jg_pf: function () {
-                return Math.round(this.amountRaised * 0.05);
+                return Math.round(this.amountRaised * this.pf.jg/100);
             },
             jg_cpf: function () {
                 return Math.round(this.amountRaised * 0.0125);
@@ -26,7 +31,7 @@ var vCalculator =
                 return this.amountRaised - (this.jg_pf + this.jg_cpf);
             },
             gfm_pf: function () {
-                return Math.round(this.amountRaised * 0.05);
+                return Math.round(this.amountRaised * this.pf.gfm/100);
             },
             gfm_cpf: function () {
                 return Math.round(this.amountRaised * 0.035 + 0.20);
